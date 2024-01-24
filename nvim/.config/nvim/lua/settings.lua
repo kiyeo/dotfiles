3b3e71bc95e2cee8b0de0176b7d79535765f0285
leo.keo <leo.keo@gentrack.com>
2024-01-24 14:44:44 +1300

    Detect genbasic file type

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -18,3 +18,49 @@ vim.opt.smartcase = true          -- except when inputting uppercase letters
 
 vim.g.netrw_liststyle = 3         -- tree style directories
 vim.g.python3_host_prog = '/usr/bin/python3.9'
+
+vim.filetype.add({
+  extension = {
+    configbundle = "json"
+  },
+  pattern = {
+    ['[.A-Z0-9]+b?'] = function(path, bufnr, ext)
+      --print(path)
+      --print(bufnr)
+      --print(ext)
+      return 'genbasic'
+    end
+  }
+})
+
+vim.lsp.set_log_level("debug")
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'genbasic',
+  callback = function()
+    vim.lsp.start({
+      name = 'genbasic-ls',
+      cmd = { 'node', '/home/leo/dev/genbasic-ls/server/out/server.js', '--stdio' },
+      root_dir = vim.fn.getcwd()
+    })
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc') -- Enable completion triggered by <c-x><c-o>
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition,
+      { desc = 'nvim-lspconfig - Press g + d to goto definition', buffer = bufnr })
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation,
+      { desc = 'nvim-lspconfig - Press g + i to goto implementation', buffer = bufnr })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references,
+      { desc = 'nvim-lspconfig - Press g + r to goto reference', buffer = bufnr })
+    vim.keymap.set('n', 'gh', vim.lsp.buf.hover,
+      { desc = 'nvim-lspconfig - Press g + h to hover', buffer = bufnr })
+    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename,
+      { desc = 'nvim-lspconfig - Press "' .. vim.g.mapleader .. '" + r + n to rename', buffer = bufnr })
+  end,
+})
+
+
+vim.api.nvim_create_autocmd("LspTokenUpdate", {
+  callback = function(args)
+    print(vim.inspect(args))
+  end,
+})
