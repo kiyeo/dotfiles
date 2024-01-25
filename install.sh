9f71bf5ec32ffff528e7da7b68df182b854781b4
kiyeo <52591219+kiyeo@users.noreply.github.com>
2024-01-25 17:04:27 +1300

    Update `.gitconfig` username and email and update `install.sh`

--- install.sh
+++ install.sh
@@ -13,7 +13,8 @@ case "$os_type" in
     distributor_id="$(lsb_release --short --id)"
     case "$distributor_id" in
       Ubuntu)
-        sudo apt-get update && sudo apt-get install -y ${apt_packages}
+        sudo apt-get update
+        sudo apt-get install -y ${apt_packages}
 	      ;;
       Gentoo)
         sudo eselect repository enable guru
@@ -27,18 +28,20 @@ case "$os_type" in
     ;;
 esac
 
-chsh -s "$(command -v zsh)"
+wget "https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.4.1/gcm-linux_amd64.2.4.1.deb" -O /tmp/gcmcore.deb
+sudo dpkg -i /tmp/gcmcore.deb
 
 wget -NP ~/.local/share/fonts/ 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFont-Regular.ttf'
+
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-
-exec /bin/zsh
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 
 if [ "${PWD##*/}" != "dotfiles" ]; then
   if [ ! -d "dotfiles" ]; then
-    git clone git@github.com:kiyeo/dotfiles.git
+    git clone https://github.com/kiyeo/dotfiles.git
   fi
   cd dotfiles
 fi
@@ -46,3 +49,8 @@ fi
 git submodule update --init --recursive --remote
 
 stow --no-folding */
+
+git-credential-manager configure
+
+chsh -s "$(command -v zsh)"
+exec "$(command -v zsh)"
